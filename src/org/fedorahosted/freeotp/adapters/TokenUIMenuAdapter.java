367adf8b635ad9b1a9b6f290034b291747643d0b
Nathaniel McCallum <npmccallum@redhat.com>
2014-03-26 15:44:23 -0400

    Implement token copy to clipboard

--- src/org/fedorahosted/freeotp/adapters/TokenUIMenuAdapter.java
+++ src/org/fedorahosted/freeotp/adapters/TokenUIMenuAdapter.java
@@ -1,23 +1,36 @@
 package org.fedorahosted.freeotp.adapters;
 
+import java.util.regex.Pattern;
+
 import org.fedorahosted.freeotp.R;
 import org.fedorahosted.freeotp.Token;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.PopupMenu;
 import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.TextView;
 
 public class TokenUIMenuAdapter extends TokenUIClickAdapter {
+    private static final Pattern PATTERN = Pattern.compile("^\\d+$");
+    private static final String TITLE = "FreeOTP Token Code";
+    private final ClipboardManager mClipboardManager;
+
     public TokenUIMenuAdapter(Activity activity) {
         super(activity);
+        mClipboardManager = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
     }
 
     @Override
     protected void bindView(View view, final int position, Token token) {
         super.bindView(view, position, token);
+
+        final TextView code = (TextView) view.findViewById(R.id.code);
 
         view.findViewById(R.id.menuButton).setOnClickListener(new OnClickListener() {
             @Override
@@ -25,10 +38,21 @@ public class TokenUIMenuAdapter extends TokenUIClickAdapter {
                 PopupMenu pm = new PopupMenu(v.getContext(), v);
                 pm.getMenuInflater().inflate(R.menu.token, pm.getMenu());
 
+                // Verify we can copy the text.
+                boolean enabled = PATTERN.matcher(code.getText()).matches();
+                pm.getMenu().findItem(R.id.action_copy).setEnabled(enabled);
+
                 pm.setOnMenuItemClickListener(new OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getItemId()) {
+                        case R.id.action_copy:
+                            if (PATTERN.matcher(code.getText()).matches()) {
+                                ClipData cd = ClipData.newPlainText(TITLE, code.getText());
+                                mClipboardManager.setPrimaryClip(cd);
+                            }
+                            break;
+
                         case R.id.action_delete:
                             TokenUIMenuAdapter.this.delete(position);
                             break;
