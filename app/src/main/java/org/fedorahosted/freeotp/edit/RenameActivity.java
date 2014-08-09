b963aae66419daf28a1a8c0013057e3d86d50ef4
Nathaniel McCallum <npmccallum@redhat.com>
2014-08-08 21:41:42 -0400

    Make the RenameActivity UI smarter

--- app/src/main/java/org/fedorahosted/freeotp/edit/RenameActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/edit/RenameActivity.java
@@ -5,13 +5,19 @@ import org.fedorahosted.freeotp.Token;
 import org.fedorahosted.freeotp.TokenPersistence;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class RenameActivity extends BaseActivity {
+
+public class RenameActivity extends BaseActivity implements TextWatcher {
     private TokenPersistence   mTokenPersistence;
     private EditText           mIssuer;
     private EditText           mLabel;
+    private Button             mRestore;
+    private Button             mSave;
 
     private void setupEditTexts(Token token) {
         mLabel.setText(token.getLabel());
@@ -27,6 +33,11 @@ public class RenameActivity extends BaseActivity {
         mTokenPersistence = new TokenPersistence(this);
         mIssuer = (EditText) findViewById(R.id.issuer);
         mLabel = (EditText) findViewById(R.id.label);
+        mRestore = (Button) findViewById(R.id.restore);
+        mSave = (Button) findViewById(R.id.save);
+
+        mIssuer.addTextChangedListener(this);
+        mLabel.addTextChangedListener(this);
 
         findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,5 +68,29 @@ public class RenameActivity extends BaseActivity {
         });
 
         setupEditTexts(mTokenPersistence.get(getPosition()));
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Token token = mTokenPersistence.get(getPosition());
+        String label = mLabel.getText().toString();
+        String issuer = mIssuer.getText().toString();
+
+        mSave.setEnabled(!label.equals(token.getLabel()) || !issuer.equals(token.getIssuer()));
+
+        token.setIssuer(null);
+        token.setLabel(null);
+
+        mRestore.setEnabled(!label.equals(token.getLabel()) || !issuer.equals(token.getIssuer()));
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
