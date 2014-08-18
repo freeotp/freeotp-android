4dec21dc757e0959ca08c41950fd498d54995d92
Nathaniel McCallum <npmccallum@redhat.com>
2014-08-18 10:53:46 -0400

    Show token image in deletion confirmation

--- app/src/main/java/org/fedorahosted/freeotp/edit/DeleteActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/edit/DeleteActivity.java
@@ -6,7 +6,10 @@ import org.fedorahosted.freeotp.TokenPersistence;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DeleteActivity extends BaseActivity {
     @Override
@@ -17,6 +20,10 @@ public class DeleteActivity extends BaseActivity {
         Token token = new TokenPersistence(this).get(getPosition());
         ((TextView) findViewById(R.id.issuer)).setText(token.getIssuer());
         ((TextView) findViewById(R.id.label)).setText(token.getLabel());
+        Picasso.with(this)
+                .load(token.getImage())
+                .placeholder(R.drawable.logo)
+                .into((ImageView) findViewById(R.id.image));
 
         findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
             @Override
