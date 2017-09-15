d9a89462209eece876c64bb144e60861bcdfbc30
eszjay <31631573+eszjay@users.noreply.github.com>
2017-09-14 22:06:22 -0500

    Use runtime permissions

--- app/src/main/java/org/fedorahosted/freeotp/MainActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/MainActivity.java
@@ -36,22 +36,29 @@
 
 package org.fedorahosted.freeotp;
 
-import org.fedorahosted.freeotp.add.AddActivity;
-import org.fedorahosted.freeotp.add.ScanActivity;
-
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.WindowManager.LayoutParams;
 import android.widget.GridView;
+import android.widget.Toast;
+
+import org.fedorahosted.freeotp.add.AddActivity;
+import org.fedorahosted.freeotp.add.ScanActivity;
 
 public class MainActivity extends Activity implements OnMenuItemClickListener {
+    private static final int PERMISSIONS_REQUEST_CAMERA = 200;
     private TokenAdapter mTokenAdapter;
     private DataSetObserver mDataSetObserver;
 
@@ -111,21 +118,55 @@ public class MainActivity extends Activity implements OnMenuItemClickListener {
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.action_scan:
-            startActivity(new Intent(this, ScanActivity.class));
-            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
-            return true;
+            case R.id.action_scan:
+                startScanActivity();
+                return true;
 
-        case R.id.action_add:
-            startActivity(new Intent(this, AddActivity.class));
-            return true;
+            case R.id.action_add:
+                startActivity(new Intent(this, AddActivity.class));
+                return true;
 
-        case R.id.action_about:
-            startActivity(new Intent(this, AboutActivity.class));
-            return true;
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
         }
 
         return false;
+    }
+
+    private void startScanActivity() {
+        //Check that Camera permission has been granted before starting the scan activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Camera permission not granted, so request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CAMERA);
+        } else {
+            //Camera permission granted, so start scan activity
+            startActivity(new Intent(this, ScanActivity.class));
+            overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result array is empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startScanActivity();
+                } else {
+                    //Camera permission was not granted. Show a message and remain in this activity
+                    showCameraRationale();
+                }
+            }
+        }
+    }
+
+    private void showCameraRationale() {
+        Toast.makeText(this, R.string.camera_justification, Toast.LENGTH_LONG).show();
     }
 
     @Override
