49b1d78c21c18e62977b56a7b08bd143f3b12863
leejunny <bungding3@naver.com>
2017-12-05 15:02:28 +0900

    USEFINGERPRINT삭제 및 상수화 포함 재 commit

--- app/src/main/java/org/fedorahosted/freeotp/CredentialManager.java
+++ app/src/main/java/org/fedorahosted/freeotp/CredentialManager.java
@@ -15,13 +15,18 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class CredentialManager {
     private static final CredentialManager ourInstance = new CredentialManager();
-    private SharedPreferences saveSetting;
+    private SharedPreferences mSaveSetting;
 
     public static CredentialManager getInstance() {
         return ourInstance;
     }
 
+    /*
+     *상수 값을 참조할 때, final로 정의함으로써, 값 참조의 효율성을 두었다.
+     */
     public static final int CREDENTIAL_CHECK = 2;
+    public static final String SETTING_ENABLE = "Enable";
+    public static final String SETTING_TIME_TYPE="Time_type";
     // 사용자가 설정할 수 있는 시간값
     public enum TimeType {
         SEC_10, SEC_30, SEC_60
@@ -31,7 +36,6 @@ public class CredentialManager {
 
     // 설정값 저장하는 변수
     private boolean mEnable = false;
-    private boolean mUseFingerprint = false;
     private int mTime = 30;
 
     // 마지막 인증 통과 시점을 저장하는 변수
@@ -43,7 +47,7 @@ public class CredentialManager {
     public int init(Context appContext) {
         mAppContext = appContext;
         mKeyguardManager = (KeyguardManager)mAppContext.getSystemService(Context.KEYGUARD_SERVICE);
-        saveSetting = mAppContext.getSharedPreferences("Setting",MODE_PRIVATE);
+        mSaveSetting = ((Activity)mAppContext).getPreferences(MODE_PRIVATE);
         if(isConfigExist() && isConfigValid()) {
             loadConfig();
         }
@@ -78,7 +82,7 @@ public class CredentialManager {
      * 기존 설정이 SharedPreference에 존재하는지 확인
      */
     private boolean isConfigExist() {
-        if(saveSetting.contains("Enable") && saveSetting.contains("Time_type"))
+        if(mSaveSetting.contains(SETTING_ENABLE) && mSaveSetting.contains(SETTING_TIME_TYPE))
             return true;
         else
             return false;
@@ -91,8 +95,7 @@ public class CredentialManager {
      */
     private boolean isConfigValid() {
         int tempTimeType = 0;
-        tempTimeType = saveSetting.getInt("Time_type",-1);
-
+        tempTimeType = mSaveSetting.getInt(SETTING_TIME_TYPE,-1);
         if(tempTimeType != 10 || tempTimeType !=20 || tempTimeType !=30)
             return false;
         else
@@ -105,14 +108,8 @@ public class CredentialManager {
      *
      */
     private  void loadConfig() {
-        mEnable = saveSetting.getBoolean("Enable",false);
-
-            if(mEnable == true)
-                mUseFingerprint = saveSetting.getBoolean("UseFingerPrint",false);
-            else
-                saveSetting.edit().putBoolean("UseFingerPrint",false);
-
-        mTime = saveSetting.getInt("Time_type",30);
+        mEnable = mSaveSetting.getBoolean(SETTING_ENABLE,false);
+        mTime = mSaveSetting.getInt(SETTING_TIME_TYPE,30);
     }
 
     /*
@@ -120,12 +117,11 @@ public class CredentialManager {
      */
     private  boolean saveConfig() {
         try {
-            if(mEnable != saveSetting.getBoolean("Enable",false))
-                saveSetting.edit().putBoolean("Enable",mEnable);
-            if(mUseFingerprint != saveSetting.getBoolean("UseFingerPrint",false))
-                saveSetting.edit().putBoolean("UseFingerPrint",mUseFingerprint);
-            if(mTime != saveSetting.getInt("Time_type",30))
-                saveSetting.edit().putInt("Time_type",mTime);
+            if(mEnable != mSaveSetting.getBoolean(SETTING_ENABLE,false))
+                mSaveSetting.edit().putBoolean(SETTING_ENABLE,mEnable);
+
+            if(mTime != mSaveSetting.getInt(SETTING_TIME_TYPE,30))
+                mSaveSetting.edit().putInt(SETTING_TIME_TYPE,mTime);
 
             return true;
         } catch (Exception e) {
@@ -136,10 +132,6 @@ public class CredentialManager {
 
     public boolean getEnable() {
         return mEnable;
-    }
-
-    public boolean getUseFingerprint() {
-        return mUseFingerprint;
     }
 
     public TimeType getTime() {
@@ -156,11 +148,6 @@ public class CredentialManager {
 
     public boolean setEnable(boolean value) {
         mEnable = value;
-        return saveConfig();
-    }
-
-    public boolean setUseFingerprint(Activity mainActivity, boolean value) {
-        mUseFingerprint = value;
         return saveConfig();
     }
 
