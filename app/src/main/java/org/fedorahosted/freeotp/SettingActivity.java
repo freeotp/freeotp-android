82e667807cc3b8e4dc2b3c1071039cb3d3e04cdc
leejunny <bungding3@naver.com>
2017-12-06 18:07:09 +0900

    Switch버튼 1차 구현(set enable)

--- app/src/main/java/org/fedorahosted/freeotp/SettingActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/SettingActivity.java
@@ -2,16 +2,36 @@ package org.fedorahosted.freeotp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 import android.view.View;
 
 public class SettingActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+
+        Switch enableLock = (Switch)findViewById(R.id.switch1);
+        enableLock.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked==true)
+                {
+                    CredentialManager.getInstance().setEnable(true);
+                }
+                else
+                    CredentialManager.getInstance().setEnable(false);
+            }
+        });
+
     }
+
 
 
 
@@ -26,6 +46,7 @@ public class SettingActivity extends Activity {
     }
     public void onClick_save(View v){
         Toast.makeText(SettingActivity.this, R.string.setting_save_message, Toast.LENGTH_LONG).show();
+        finish();
     }
 
 }
