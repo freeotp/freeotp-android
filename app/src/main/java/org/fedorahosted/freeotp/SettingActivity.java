b4e3043b0d73a774bbdca317de633008a69c0380
Kang <kangkang1104@naver.com>
2017-12-06 17:25:43 +0900

    각 버튼에 대한 토스트 메시지 추가

--- app/src/main/java/org/fedorahosted/freeotp/SettingActivity.java
+++ app/src/main/java/org/fedorahosted/freeotp/SettingActivity.java
@@ -2,6 +2,8 @@ package org.fedorahosted.freeotp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
 
 public class SettingActivity extends Activity {
 
@@ -10,4 +12,20 @@ public class SettingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
     }
+
+
+
+    public void onClick_10sec(View v){
+        Toast.makeText(SettingActivity.this, R.string.setting_10sec_message, Toast.LENGTH_LONG).show();
+    }
+    public void onClick_30sec(View v){
+        Toast.makeText(SettingActivity.this, R.string.setting_30sec_message, Toast.LENGTH_LONG).show();
+    }
+    public void onClick_60sec(View v){
+        Toast.makeText(SettingActivity.this, R.string.setting_60sec_message, Toast.LENGTH_LONG).show();
+    }
+    public void onClick_save(View v){
+        Toast.makeText(SettingActivity.this, R.string.setting_save_message, Toast.LENGTH_LONG).show();
+    }
+
 }
