64ea85a78a656a70e543cc87257ec531efc50bf3
Justin Stephenson <jstephen@redhat.com>
2022-07-15 10:27:58 -0400

    Add Onboarding Information

---
+++ mobile/src/main/java/org/fedorahosted/freeotp/BackupsFragment.java
@@ -0,0 +1,40 @@
+package org.fedorahosted.freeotp;
+
+import static org.fedorahosted.freeotp.OnBoardingActivity.COMPLETED_ONBOARDING;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+import android.preference.PreferenceManager;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class BackupsFragment extends Fragment implements View.OnClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_backups, container, false);
+
+        TextView textView = view.findViewById(R.id.textViewGoogle);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Button doneButton = view.findViewById(R.id.button_onboard_done);
+        doneButton.setOnClickListener(this::onboardDone);
+        return view;
+    }
+
+    public void onboardDone(View view) {
+        SharedPreferences.Editor sharedPreferencesEditor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+        sharedPreferencesEditor.putBoolean(COMPLETED_ONBOARDING, true).apply();
+        requireActivity().finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+}
---
+++ mobile/src/main/java/org/fedorahosted/freeotp/KeystoreFragment.java
@@ -0,0 +1,15 @@
+package org.fedorahosted.freeotp;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KeystoreFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_keystore, container, false);
+    }
+}
