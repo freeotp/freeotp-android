9891008000e8cec6d9739452607cb57db5074519
Mubashshir <ahmubashshir@gmail.com>
2026-02-17 01:05:51 +0600

    Replace deprecated back gesture with predictive back
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- mobile/src/main/java/org/fedorahosted/freeotp/OnBoardingActivity.java
+++ mobile/src/main/java/org/fedorahosted/freeotp/OnBoardingActivity.java
@@ -1,11 +1,15 @@
 package org.fedorahosted.freeotp;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
+
 import com.google.android.material.tabs.TabLayout;
 
 public class OnBoardingActivity extends AppCompatActivity {
@@ -55,18 +59,22 @@ public class OnBoardingActivity extends AppCompatActivity {
                 tabLayout.selectTab(tabLayout.getTabAt(position));
             }
         });
-    }
 
-    @Override
-    public void onBackPressed() {
-        if (viewPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-        }
+        getOnBackPressedDispatcher().addCallback(this,
+            new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (viewPager.getCurrentItem() == 0) {
+                    // If the user is currently looking at the first step, allow the system to handle the
+                    // Back button. This calls finish() on this activity and pops the back stack.
+                    setEnabled(false);
+                    getOnBackPressedDispatcher().onBackPressed();
+                } else {
+                    // Otherwise, select the previous step.
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+                }
+            }
+        });
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
@@ -75,6 +83,7 @@ public class OnBoardingActivity extends AppCompatActivity {
         }
 
         @Override
+        @NonNull
         public Fragment createFragment(int position) {
             switch (position) {
                 case 0:
